29112da4bc7c7f5ae2cc5143a9e435e37ce98a2f
May <zfuak@connect.ust.hk>
2024-05-30 08:56:54 +0200

    tried poisson but failed

--- Code/REBayes.R
+++ Code/REBayes.R
@@ -2,33 +2,62 @@
 rm(list = ls())
 pacman::p_load(REBayes, data.table, lattice, ggplot2)
 reg_inf_ols_FI <- readRDS("Results/2016-2022/reg_inf_ols_FI.rds")
-pdt <- readRDS("Results/2016-2022/pdt_ols.rds")
-# pdt[,`:=`(Mean_res=mean(Res)),by=.(FI)] # not needed
+summary(reg_inf_ols_FI)
+pdt <- readRDS("Results/2016-2022/pdt_inf_ols.rds")
+
 # sufficient statistics for \theta_i
 pdt[, `:=`(hat_mu = FixedEffect + Res), by = .(FI)]
-
 # sufficient statistics for \sigma_i^2
-pdt[, `:=`(Var_res = var(Res)), by = .(FI)]
+pdt[, `:=`(Var_res1 = var(Res)), by = .(FI)]
+# another way to estimate the variance
+pdt[, `:=`(Mean_res = mean(Res)), by = .(FI)]
+pdt[, `:=`(Demeaned_res = Res - Mean_res), by = .(FI)]
+pdt[, `:=`(Var_res2 = var(Demeaned_res)), by = .(FI)]
 
 pdt[, Nobs := .N, by = .(FI)]
 pdt <- pdt[Nobs >= 5]
 pdt[, id := as.numeric(as.factor(FI))]
-
+dt1 <- pdt[, .(AN, FI, id, SEJHC_MCO, SEJHP_MCO, SEANCES_MED, ETP_INF, EFF_MD, FixedEffect, Res, hat_mu, Var_res1, Var_res2, N)]
+dim(dt) # 9130 13
 
 # three ways to estimate the mixing density
+help(GLmix)
+
+# GLmix
+dt2 <- pdt[, .(hat_mu = first(hat_mu), Var_res1 = first(Var_res1), Var_res2 = first(Var_res2), Nobs = first(Nobs)), by = .(FI)]
+f <- GLmix(dt$hat_mu, verb = 5)
+pdf("Figures/2016-2022/GLmix.pdf", width = 8, height = 5)
+plot(f, xlab = expression(mu), main = "Estimated Location Mixing Density")
+dev.off()
+
 # GLVmix
 help(GLVmix)
-dt <- pdt[, .(hat_mu = first(hat_mu), Var_res = first(Var_res)), by = .(FI)]
-dt <- dt[dt$Var_res > 0, ]
-saveRDS(dt, "Results/2016-2022/theta_sigma.rds")
+dt2 <- pdt[, .(hat_mu = first(hat_mu), Var_res1 = first(Var_res1), Var_res2 = first(Var_res2), Nobs = first(Nobs)), by = .(FI)]
+saveRDS(dt2, "Results/2016-2022/theta_sigma.rds")
+f <- GLVmix(t = dt2$hat_mu, s = dt2$Var_res1, m = dt2$Nobs)
+require(lattice) # a visualisation package
+str(f)
+g <- expand.grid(alpha = f$u, theta = f$v)
+g$fuv <- f$fuv
+pl <- cloud(fuv ~ alpha * theta,
+    data = g, type = "h", lwd = 2,
+    zlim = c(0, max(g$fuv)), scales = list(
+        arrows = FALSE,
+        xlab = expression(alpha), ylab = expression(theta), zlab = "density",
+        screen = list(z = 10, x = -70)
+    )
+)
+print(pl)
 
+# TGLVmix
 
 # WGLVmix
 help(WGLVmix)
 
 
-f <- WGLVmix(y = pdt$hat_mu, id = pdt$id, verb = 5)
+f <- WGLVmix(y = pdt$hat_mu, id = pdt$id)
 require(lattice) # a visualisation package
+str(f)
 g <- expand.grid(alpha = f$u, theta = f$v)
 g$fuv <- f$fuv
 
@@ -48,7 +77,7 @@ dev.off()
 
 # WTGVmix
 help(WTLVmix)
-f_t <- WTLVmix(y = pdt$hat_mu, id = pdt$id, u = 500, v = 300)
+f_t <- WTLVmix(y = pdt$hat_mu, id = pdt$id, u = 300, v = 300)
 
 # plot(x=f_t$u,y=f_t$fu, xlab = expression(mu), main = "Estimated Location Mixing Density")
 X11(width = 8, height = 5)
--- Code/RegResults.R
+++ Code/RegResults.R
@@ -1,17 +1,37 @@
+rm(list = ls())
 source("Code/RegX.R")
 
 # For registered nurses
 dt <- readRDS("Data/Out/combineddata_2016_2022.rds")
+# hospitals that have changed status
 FI_stat_change <- unique(dt[, .(FI, STJR)])
 FI_stat_change <- FI_stat_change[, .N, by = .(FI)]
 FI_stat_change <- FI_stat_change[N > 1]
 dt <- dt[!FI %in% FI_stat_change$FI]
+# those observations with possible coding errors...
+# 2022 760000166
+# 2016 910001973
+dt <- dt[!(FI == 760000166 & AN == 2022) | !(FI == 910001973 & AN == 2016)]
+# dt[,Y:=ETP_INF*EFF_MD]
+dt <- dt[ETP_INF > 0]
+varr1 <- c("SEJHC_MCO", "SEJHP_MCO", "SEANCES_MED")
+varl <- "ETP_INF"
+dt <- dt[SEJHC_MCO > 1 | SEJHP_MCO > 1 | SEANCES_MED > 1]
+dt[, (varr1) := lapply(.SD, function(x) x <- x + 1), .SDcols = varr1]
+dt[, Nobs := .N, by = .(FI)]
+dt <- dt[Nobs == 6]
+length(unique(dt$FI))
+# dt[, (varr1) := lapply(.SD, function(x) ifelse(x == 0, 1, x)), .SDcols = varr1] # add one to ever column to avoid zero value issue
 
 pdt <- panel(dt, panel.id = ~ FI + AN, time.step = "consecutive", duplicate.method = "first")
-pdt[, `:=`(SEJHC_MCO_l1 = l(SEJHC_MCO, 1), SEJHP_MCO_l1 = l(SEJHP_MCO, 1), SEANCES_MED_l1 = l(SEANCES_MED, 1))]
+# construct lagged variables
+# pdt[, `:=`(SEJHC_MCO_l1 = l(SEJHC_MCO, 1), SEJHP_MCO_l1 = l(SEJHP_MCO, 1), SEANCES_MED_l1 = l(SEANCES_MED, 1))]
 
-varr1 <- c("SEJHC_MCO", "SEJHP_MCO", "SEANCES_MED")
-reg_inf_ols_FI <- reg_X(pdt, "ETP_INF", varr1)
+
+
+# OLS regression
+reg_inf_ols_FI <- reg_X(pdt, varl, varr1)
+summary(reg_inf_ols_FI)
 # add residuals to the panel
 obs_removed <- reg_inf_ols_FI$obs_selection$obsRemoved * (-1)
 pdt_used <- pdt[!obs_removed, ]
@@ -20,9 +40,93 @@ pdt_used[, `:=`(Res = residuals)] # add residuals to the panel
 # add fixed effect to the panel
 FE <- reg_inf_ols_FI$sumFE
 pdt_used[, `:=`(FixedEffect = FE)]
-saveRDS(pdt_used, "Results/2016-2022/pdt_ols.rds")
+saveRDS(pdt_used, "Results/2016-2022/pdt_inf_ols.rds")
 saveRDS(reg_inf_ols_FI, "Results/2016-2022/reg_inf_ols_FI.rds")
+# plot
+reg_inf_ols_FI <- readRDS("Results/2016-2022/reg_inf_ols_FI.rds")
+status <- readRDS("Data/Out/status_stable_2016_2022.rds")
+plot_FE(reg_inf_ols_FI, "FI", status)
+
+
+# dropping CHU and PNL
+reg_inf_ols_FI_pub <- reg_X(pdt[STJR == 1 | STJR == 2], varl, varr1)
+summary(reg_inf_ols_FI_pub)
+plot_FE(reg_inf_ols_FI_pub, "FI", status)
 
 # For medical doctors
-reg_md_olf_FI <- reg_X(pdt, "EFF_MD", varr1)
-saveRDS(reg_md_olf_FI, "Results/2016-2022/reg_md_olf_FI.rds")
+reg_md_ols_FI <- reg_X(pdt, "EFF_MD", varr1)
+# add residuals to the panel
+obs_removed <- reg_md_ols_FI$obs_selection$obsRemoved * (-1)
+pdt_used <- pdt[!obs_removed, ]
+residuals <- reg_md_ols_FI$residuals
+pdt_used[, `:=`(Res = residuals)] # add residuals to the panel
+# add fixed effect to the panel
+FE <- reg_md_ols_FI$sumFE
+pdt_used[, `:=`(FixedEffect = FE)]
+saveRDS(pdt_used, "Results/2016-2022/pdt_md_ols.rds")
+saveRDS(reg_md_ols_FI, "Results/2016-2022/reg_md_ols_FI.rds")
+
+# plot
+reg_md_ols_FI <- readRDS("Results/2016-2022/reg_md_ols_FI.rds")
+status <- readRDS("Data/Out/status_stable_2016_2022.rds")
+plot_FE(reg_md_ols_FI, "FI", status)
+
+
+
+# Multiplicative model Pseudo Poisson regression
+# If we actually assume Poisson distribution of the multiplicative error term, we are in the case of heterogeneous known variance, where the variance stabilizing transformation gives $z_{it} =\sqrt{\frac{y_it}{m_{it}}} \sim \caln (\theta_i, \frac{1}{w_{it}}= \frac{1}{4m_{it}})$
+
+# 1. estimate m_{it}
+reg_inf_pois_FI <- reg_X(pdt, varl, varr1, method = "pois")
+summary(reg_inf_pois_FI)
+check <- data.table()
+check[, test1 := reg_inf_pois_FI$fitted.values + reg_inf_pois_FI$residuals]
+check[, y := pdt$ETP_INF]
+
+coef <- as.matrix(reg_inf_pois_FI$coefficients)
+x <- as.matrix(pdt[, .(log(SEJHC_MCO), log(SEJHP_MCO), log(SEANCES_MED), CASEMIX)])
+m_it <- exp(x %*% coef)
+
+check[, test2 := m_it * exp(reg_inf_pois_FI$sumFE) + reg_inf_pois_FI$residuals]
+check[, fe := exp(reg_inf_pois_FI$sumFE)]
+check[, m_it := m_it]
+View(check)
+# this is so stupid...it's like everything is fixed effect...
+
+panel <- data.table(FI = pdt$FI, AN = pdt$AN, m_it = m_it, y_it = pdt$ETP_INF, FE = exp(reg_inf_pois_FI$sumFE))
+# 2. construct z_{it} =\sqrt{\frac{y_it}{m_{it}}}
+panel[, `:=`(w_it = (4 * m_it))]
+panel[, `:=`(z_it = sqrt(y_it / m_it))]
+# 3. construct T_i= \sum w_{it}z_{it}/w_i \sim N(\theta_i, 1/w_i)
+T_i <- panel[, .(T_i = sum(z_it / w_it) / sum(w_it), FE, w_i = sum(w_it)), by = .(FI)]
+View(T_i[, .(T_i, 1 / sqrt(w_i))])
+saveRDS(T_i, "Results/2016-2022/T_i.rds")
+# 3.2 construct a data.frame to feed into the fit1d function
+
+# to transform the data.frame into a matrix of 3d dimension [1:1526,1:6,1:2]
+d <- panel[, .(FI, AN, w_it, z_it, y_it, m_it)]
+d_vec <- as.vector(t(d[, .(y_it, m_it)]))
+dim1 <- length(unique(d$FI))
+dim2 <- length(unique(d$AN))
+dim3 <- 2
+dim <- c(dim1, dim2, dim3)
+d_mat <- array(d_vec, dim)
+saveRDS(d_mat, "Results/2016-2022/d_mat.rds")
+
+# 4. NPMLE to estimate G while the variance is known and is equal to 1/4m_{it}
+# We can input the T_i$T_i and variance as 1/T_i$w_i
+# Then we can estimate G by NPMLE
+pacman::p_load(REBayes)
+# help(GLmix)
+f <- GLmix(x = T_i$T_i, v = 300, sigma = 1 / sqrt(T_i$w_i))
+str(f)
+pdf("Figures/2016-2022/GLmix_pois.pdf", width = 8, height = 5)
+plot(f, xlab = expression(mu), main = "Estimated Location Mixing Density")
+dev.off()
+
+fs <- KWsmooth(f, bw)
+pdf("Figures/2016-2022/GLmix_pois_smoothed.pdf", width = 8, height = 5)
+plot(fs, xlab = expression(mu), main = "Estimated Smoothed Location Mixing Density")
+dev.off()
+
+# Note: The distribution of the statistics T_i is a convolution of G and the normal distribution N(\theta_i, 1/w_i))
