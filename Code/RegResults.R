82545ac00042e350962e16072e81eabce2cebe66
maayyy <zfuak@connect.ust.hk>
2024-06-15 08:24:47 +0200

    whether to combine SEJHC and SEJHP

--- Code/RegResults.R
+++ Code/RegResults.R
@@ -2,20 +2,38 @@ rm(list = ls())
 pacman::p_load(data.table, plm, texreg, fixest)
 source("Code/RegX_fixest.R")
 source("Code/RegX_plm.R")
-
+colnames(dt_inf)
 dt_inf <- readRDS("Data/Out/dt_inf.rds")
 dt_inf[, SEJ_MCO := SEJHC_MCO + SEJHP_MCO]
 dt_inf[, SEJ_PSY := VEN_TOT + SEJ_HTP_TOT]
-
 dt_inf[, MCO := (SEJHC_MCO > 1)]
+dt_inf[, PSY := (SEJ_PSY > 2)]
+level_order <- c(1, 2, 3, 0) # use public as base level
+dt_inf$STJR <- factor(dt_inf$STJR, levels = level_order)
+dt_inf <- dt_inf[SEJHC_MCO > 1 | SEJHP_MCO > 1 | SEANCES_MED > 1]
 colnames(dt_inf)
 # replicate the regression in Table 6 col 4
 
+var_input <- c("ETP_INF")
+var_output <- c("SEJHC_MCO", "SEJHP_MCO", "SEANCES_MED", "PLA_MCO")
+var_control <- c("STJR")
+
+spec_pool <- as.formula(paste0(add_log(var_input), "~", paste(c(add_log(var_output), var_control), collapse = "+")))
+print(spec_pool)
+dim(dt_inf[AN <= 2019 & AN >= 2016])
+reg_pool <- lm(spec_pool, data = dt_inf)
+reg_pool <- feols(spec_pool, data = dt_inf[AN <= 2019 & AN >= 2016])
+summary(reg_pool)
+help(plm)
+summary(reg_pool)
+help(plm)
+
+
 varl <- "ETP_INF"
 lhs <- add_log(varl)
-varr <- c("SEJ_MCO", "SEANCES_MED", "PASSU")
-rhs_x <- paste(c(add_log(varr), "CASEMIX"), collapse = " + ")
-varr_z <- c("SEJ_MCO", "SEANCES_MED", "PASSU") # "ETP_INF"
+varr <- c("SEJHC_MCO", "SEJHP_MCO", "SEANCES_MED")
+rhs_x <- paste(c(add_log(varr)), collapse = " + ")
+varr_z <- c("SEJHC_MCO", "SEJHP_MCO", "SEANCES_MED") # "ETP_INF"
 rhs_z <- paste(add_lag(add_log(varr_z), 2), collapse = " + ")
 rhs <- paste(rhs_x, "|", rhs_z) # "+lag(log(ETP_INF),1)",
 rhs
